a5a2e7a6fb3fcd0450d7a82ba990673d5e8467a6
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-08-02 15:50:51 -0500

    feature: starting to extend the API

---
+++ Data/Seed.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+        public Seed(DataContext context)
+        {
+            _context = context;   
+        }
+
+        public void SeedUsers()
+        {
+            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+            var users = JsonConvert.DeserializeObject<List<User>>(userData);
+            foreach (var user in users)
+            {
+                byte[] passwordHash, passwordSalt;
+                CreatePasswordHash("password", out passwordHash, out passwordSalt);
+            }
+        }
+    }
+}
