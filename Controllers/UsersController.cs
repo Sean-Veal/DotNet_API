6e8bcafa4fb2ab84c2f95e7d9a51370906fdab1c
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-08-06 15:54:26 -0500

    feature: created DTOs for mapping sensitive data

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,35 +1,39 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
+using DatingApp.API.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DatingApp.API.Controllers
-{
+namespace DatingApp.API.Controllers {
     [Authorize]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
-    {
+    [Route ("api/[controller]")]
+    public class UsersController : ControllerBase {
         private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
 
-        public UsersController(IDatingRepository repo)
-        {
+        public UsersController (IDatingRepository repo, IMapper mapper) {
+            _mapper = mapper;
             _repo = repo;
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
-        {
-            var users = await _repo.GetUsers();
+        public async Task<IActionResult> GetUsers () {
+            var users = await _repo.GetUsers ();
 
-            return Ok(users);
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDTO>>(users);
+
+            return Ok (usersToReturn);
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetUser(int id)
-        {
-            var user  = await _repo.GetUser(id);
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetUser (int id) {
+            var user = await _repo.GetUser (id);
 
-            return Ok(user);
+            var userToReturn = _mapper.Map<UserForDetailedDTO>(user);
+
+            return Ok (userToReturn);
         }
     }
 }
